00a634629da202910c9c972eeea947317a751897
yopijay <judan.pauljohn@gmail.com>
2020-02-27 11:59:42 +0800

    Switch Toggle
    Standard design for Switch Toggle

--- resources/views/layout/switch-toggle.blade.php
+++ resources/views/layout/switch-toggle.blade.php
@@ -64,6 +64,96 @@
                     <label for= "toggle-ios7"></label>
                 </div>
             </div>
+            <div class= "col-md-3">
+                <div class= "toggle toggle-skewed t-primary">
+                    <input type= "checkbox" id= "toggle-s1">
+                    <label for= "toggle-s1" data-tg-off= "OFF" data-tg-on= "ON"></label>
+                </div>
+                <div class= "toggle toggle-skewed t-success">
+                    <input type= "checkbox" id= "toggle-s2">
+                    <label for= "toggle-s2" data-tg-off= "OFF" data-tg-on= "ON"></label>
+                </div>
+                <div class= "toggle toggle-skewed t-danger">
+                    <input type= "checkbox" id= "toggle-s3">
+                    <label for= "toggle-s3" data-tg-off= "OFF" data-tg-on= "ON"></label>
+                </div>
+                <div class= "toggle toggle-skewed t-warning">
+                    <input type= "checkbox" id= "toggle-s4">
+                    <label for= "toggle-s4" data-tg-off= "OFF" data-tg-on= "ON"></label>
+                </div>
+                <div class= "toggle toggle-skewed t-info">
+                    <input type= "checkbox" id= "toggle-s5">
+                    <label for= "toggle-s5" data-tg-off= "OFF" data-tg-on= "ON"></label>
+                </div>
+                <div class= "toggle toggle-skewed t-light">
+                    <input type= "checkbox" id= "toggle-s6">
+                    <label for= "toggle-s6" data-tg-off= "OFF" data-tg-on= "ON"></label>
+                </div>
+                <div class= "toggle toggle-skewed t-dark">
+                    <input type= "checkbox" id= "toggle-s7">
+                    <label for= "toggle-s7" data-tg-off= "OFF" data-tg-on= "ON"></label>
+                </div>
+            </div>
+            <div class= "col-md-3">
+                <div class= "toggle toggle-flat t-primary">
+                    <input type= "checkbox" id= "toggle-f1">
+                    <label for= "toggle-f1" data-tg-off= "OFF" data-tg-on= "ON"></label>
+                </div>
+                <div class= "toggle toggle-flat t-success">
+                    <input type= "checkbox" id= "toggle-f2">
+                    <label for= "toggle-f2" data-tg-off= "OFF" data-tg-on= "ON"></label>
+                </div>
+                <div class= "toggle toggle-flat t-danger">
+                    <input type= "checkbox" id= "toggle-f3">
+                    <label for= "toggle-f3" data-tg-off= "OFF" data-tg-on= "ON"></label>
+                </div>
+                <div class= "toggle toggle-flat t-warning">
+                    <input type= "checkbox" id= "toggle-f4">
+                    <label for= "toggle-f4" data-tg-off= "OFF" data-tg-on= "ON"></label>
+                </div>
+                <div class= "toggle toggle-flat t-info">
+                    <input type= "checkbox" id= "toggle-f5">
+                    <label for= "toggle-f5" data-tg-off= "OFF" data-tg-on= "ON"></label>
+                </div>
+                <div class= "toggle toggle-flat t-light">
+                    <input type= "checkbox" id= "toggle-f6">
+                    <label for= "toggle-f6" data-tg-off= "OFF" data-tg-on= "ON"></label>
+                </div>
+                <div class= "toggle toggle-flat t-dark">
+                    <input type= "checkbox" id= "toggle-f7">
+                    <label for= "toggle-f7" data-tg-off= "OFF" data-tg-on= "ON"></label>
+                </div>
+            </div>
+            <div class= "col-md-4 offset-md-4 mt-5">
+                <div class= "toggle toggle-flip t-primary">
+                    <input type= "checkbox" id= "toggle-fl1">
+                    <label for= "toggle-fl1" data-tg-off= "OFF" data-tg-on= "ON"></label>
+                </div>
+                <div class= "toggle toggle-flip t-success">
+                    <input type= "checkbox" id= "toggle-fl2">
+                    <label for= "toggle-fl2" data-tg-off= "OFF" data-tg-on= "ON"></label>
+                </div>
+                <div class= "toggle toggle-flip t-danger">
+                    <input type= "checkbox" id= "toggle-fl3">
+                    <label for= "toggle-fl3" data-tg-off= "OFF" data-tg-on= "ON"></label>
+                </div>
+                <div class= "toggle toggle-flip t-warning">
+                    <input type= "checkbox" id= "toggle-fl4">
+                    <label for= "toggle-fl4" data-tg-off= "OFF" data-tg-on= "ON"></label>
+                </div>
+                <div class= "toggle toggle-flip t-info">
+                    <input type= "checkbox" id= "toggle-fl5">
+                    <label for= "toggle-fl5" data-tg-off= "OFF" data-tg-on= "ON"></label>
+                </div>
+                <div class= "toggle toggle-flip t-light">
+                    <input type= "checkbox" id= "toggle-fl6">
+                    <label for= "toggle-fl6" data-tg-off= "OFF" data-tg-on= "ON"></label>
+                </div>
+                <div class= "toggle toggle-flip t-dark">
+                    <input type= "checkbox" id= "toggle-fl7">
+                    <label for= "toggle-fl7" data-tg-off= "OFF" data-tg-on= "ON"></label>
+                </div>
+            </div>
         </div>
     </div>
 </div>
